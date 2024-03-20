312133fa24f91ebff6ea7977be262b1cc26e31c0
andreia <andreia.de-almeida@ipsa.fr>
2024-03-20 09:33:44 +0100

    added file

---
+++ print_values_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    // Loop while i is less than or equal to 100
+    while (i <= 100)
+    {
+        // Print the current value of i
+        printf("%d\n", i);
+
+        // Increment i by 1
+        i++;
+    }
+
+    return 0;
+}
