d712af941b5dee302c092fc8e4729ea23b7dc1ad
andreia <andreia.de-almeida@ipsa.fr>
2024-03-20 09:38:19 +0100

    added file

---
+++ print_value_address.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare an integer variable and assign it a value
+    int num = 42;
+
+    // Print the value of the variable
+    printf("Value of num: %d\n", num);
+
+    // Print the memory address of the variable
+    printf("Memory address of num: %p\n", &num);
+
+    return 0;
+}
