7bdc0e22152375050f46c353f716c3f1a256ff94
andreia <andreia.de-almeida@ipsa.fr>
2024-03-20 09:33:24 +0100

    added file

---
+++ print_values.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    // Loop from 1 to 100
+    for (i = 1; i <= 100; i++)
+    {
+        // Print the current value of i
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
